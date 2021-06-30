0cbb4164e45e092ba4cba4157a0079c7e9e8e60c
ss <setrotec@outlook.com>
2021-06-30 18:24:42 -0300

    Criando excecoes

--- src/Fluxo.java
+++ src/Fluxo.java
@@ -3,18 +3,19 @@ public class Fluxo {
     public static void main(String[] args) {
         System.out.println("Ini do main");
         
-        /* ---------------------------------------------------------------------------------    
+    	/* ---------------------------------------------------------------------------------    
     		Exceções Java, trantando erro ArithmeticException 
       		A instrução try permite definir um bloco de código a ser testado 
      		quanto a erros enquanto está sendo executado.
-        */
+    	 */
        try {
     	   metodo1();
     	   
-    	 /* ---------------------------------------------------------------------------------   
-         	A instrução catch permite definir um bloco de código a ser executado, 
- 			se ocorrer um erro no bloco try.*/
-	   } catch(ArithmeticException | NullPointerException  ex) {
+    	  /*----------------------------------------------------------------------------------   
+         	 A instrução catch permite definir um bloco de código a ser executado, 
+ 			 se ocorrer um erro no bloco try.
+ 		   */
+	   } catch(ArithmeticException | NullPointerException | MinhaExcecao ex) {
 		   
 		   /*--------------------------------------------------------------------------------
 		     Acessando msg de erro dentro do objeto ArithmeticException
@@ -22,9 +23,11 @@ public class Fluxo {
 		    String msg = ex.getMessage();
 		    
 	    	System.out.println("Exception " + msg);
+	    	
 	    	/*-------------------------------------------------------------------------------
-	    	Acessando informação da pilha guradada dentro do objeto ArithmeticException
-		     com metodo .printStackTrace(*/
+	    	  Acessando informação da pilha guradada dentro do objeto ArithmeticException
+		      com metodo .printStackTrace()
+		     */
 	    	ex.printStackTrace();
 	   }
         System.out.println("Fim do main");
@@ -43,10 +46,9 @@ public class Fluxo {
         
         /*------------------------------------------------------------------------------------
           	trow = jogar na pilha, 
-          	Instanciando um objeto apartir da classe ArithmeticException
-         	passando mensagem de erro como parametro. 
+          	Instanciando um objeto apartir da classe MinhaExcecao
          */
-        throw new ArithmeticException("Erro ao executar ");
+        throw new MinhaExcecao ("Erro");
         //System.out.println("Fim do metodo2");
      }    
  }	
